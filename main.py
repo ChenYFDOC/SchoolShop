924a8fce56a455d1cbbbf1148589480994b4047f
ChenYFDOC <986940788@qq.com>
2021-08-09 00:49:50 +0800

    修复换行符

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from tornado import web, ioloop
-from settings import settings
-from urls import url_match
-
-if __name__ == '__main__':
-    app = web.Application([
-        *url_match,
-        (r'/media/(.*)', web.StaticFileHandler, {'path': settings['media_path']}, 'media')
-    ], **settings)
-    app.listen(8080)
-    ioloop.IOLoop.current().start()
+# from tornado import web, ioloop
+# from settings import settings
+# from urls import url_match
+#
+# if __name__ == '__main__':
+#     app = web.Application([
+#         *url_match,
+#         (r'/media/(.*)', web.StaticFileHandler, {'path': settings['media_path']}, 'media')
+#     ], **settings)
+#     app.listen(8080)
+#     ioloop.IOLoop.current().start()
+a = 1 or 0
+# print(True+'asd')
+print(a)
